bc408da347dbe30bcb91e535ebb83cd4e10244e3
Greg Emerick <gemerick@gmail.com>
2022-11-09 22:57:18 -0600

    Add build args and quiet downloads

--- fetchSoftware.sh
+++ fetchSoftware.sh
@@ -12,17 +12,17 @@ cd sw
 
 # https://wpilib.screenstepslive.com/s/currentCS/m/cpp/l/1027500-installing-c-and-java-development-tools-for-frc
 # https://github.com/wpilibsuite/vscode-wpilib/releases
-wget https://github.com/wpilibsuite/allwpilib/releases/download/v${wpilib}/WPILib_Linux-${wpilib}.tar.gz
+wget -q https://github.com/wpilibsuite/allwpilib/releases/download/v${wpilib}/WPILib_Linux-${wpilib}.tar.gz
 
 #https://wpilib.screenstepslive.com/s/currentCS/m/cpp/l/1035723-3rd-party-libraries
 # http://www.ctr-electronics.com/control-system/hro.html#product_tabs_technical_resources
-wget https://github.com/CrossTheRoadElec/Phoenix-Releases/releases/download/v${ctrePath}/CTRE_Phoenix_FRC_Linux_${ctreZip}.zip
+wget -q https://github.com/CrossTheRoadElec/Phoenix-Releases/releases/download/v${ctrePath}/CTRE_Phoenix_FRC_Linux_${ctreZip}.zip
 # wget https://github.com/CrossTheRoadElec/Phoenix-Releases/releases/download/Phoenix_v${ctrePath}/CTRE_Phoenix_FRC_nonWindows_${ctreZip}.zip
 
 #http://www.revrobotics.com/sparkmax-software/
 # https://docs.revrobotics.com/sparkmax/software-resources/spark-max-api-information
-wget http://www.revrobotics.com/content/sw/max/sdk/SPARK-MAX-SDK-v${max}.zip -O SPARK-MAX-SDK-v${max}.zip
-wget https://github.com/REVrobotics/REV-Software-Binaries/releases/download/revlib-${rev}/REVLib-offline-v${rev}.zip
+# wget http://www.revrobotics.com/content/sw/max/sdk/SPARK-MAX-SDK-v${max}.zip -O SPARK-MAX-SDK-v${max}.zip
+wget -q https://github.com/REVrobotics/REV-Software-Binaries/releases/download/revlib-${rev}/REVLib-offline-v${rev}.zip
 
 mkdir vendor
 cd vendor
